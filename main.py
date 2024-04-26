792e0d9dfa6098c195574ad0c5f3e6f117493ec3
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 22:34:39 +0700

    Less than operator (<)

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 if __name__ == "__main__":
-  print("Python Comparison Operators")
+  # Less than operator (<)
+
+  """
+  left_value < right_value
+  """
+
+  print(10 < 20)
+  print(30 < 20)
+
+  print("apple" < "banana")
+  print("banana" < "apple")
+
+  """
+  The expression "apple" < "banana" returns True because the letter "a" in "apple" is before the letter "o" in "orange".
+  Similarly, the "banana" < "apple" returns False because the letter "b" in "banana" is after the letter "a"
+  """
+
+  x = 10
+  y = 20
+  print(x < y)
+  print(y < x)
